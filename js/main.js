2c237a3cbec7d3f4d102eda159b16dbc5a208069
ar4ibais <ak.hutov@yandex.ru>
2023-07-06 21:32:33 +0400

    made adaptive for header

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 $(function() {
     Fancybox.bind();
 
+    $('.header__mob-btn').on('click', function() {
+        $('.menu').toggleClass('active')
+    })
+
     $('.blog__slider').slick({
         arrows: false,
         dots: true
-    })
-
-
+    });
 
     var mixer = mixitup('.gallery__images');
 });
