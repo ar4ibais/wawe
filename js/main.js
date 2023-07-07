e06fe12f2ea20b03f0b2fe6a6f72bd1308e46c93
ar4ibais <ak.hutov@yandex.ru>
2023-07-08 00:57:04 +0400

    fixed header background

--- js/main.js
+++ js/main.js
@@ -3,12 +3,23 @@ $(function() {
 
     $('.header__mob-btn').on('click', function() {
         $('.menu').toggleClass('active')
+        $('body').toggleClass('active')
     })
 
     $('.blog__slider').slick({
         arrows: false,
         dots: true
     });
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= 50) {
+            // document.querySelector('.header__top').classList.addClass('active')
+            $('.header__top').addClass('active')
+        } else {
+            // document.querySelector('.header__top').classList.removeClass('active')
+            $('.header__top').removeClass('active')
+        }
+    })
 
     var mixer = mixitup('.gallery__images');
 });
